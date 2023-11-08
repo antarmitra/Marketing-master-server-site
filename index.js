6eaf55ccd904b32074dcf442e4b367bb4ac89297
AntarProgrammer <antarmitra33@gmail.com>
2023-11-08 22:57:27 +0600

    remove .env and .gitignore file

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_pass}@cluster0.orneeg0.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
